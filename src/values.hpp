a7f5324123cdfc3e4b12f2c4096038b4b83015fc
Stan Serebryakov <i@cfr.pw>
2026-03-26 20:58:44 +0300

    Add values.hpp

---
+++ src/values.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#define GLM_FORCE_PRECISION_HIGHP_INT
+#define GLM_FORCE_PRECISION_HIGH_FLOAT
+#define GLM_FORCE_PRECISION_HIGHP_DOUBLE
+
+#include <glm/glm.hpp>
+#include <glm/gtc/type_precision.hpp>
+
+namespace raytracer {
+
+using Float = glm::float64_t;
+
+using Vec3 = glm::tvec3<Float, glm::defaultp>;
+using Vec4 = glm::tvec4<Float, glm::defaultp>;
+
+using Transform = glm::tmat4x4<Float, glm::defaultp>;
+
+using Color = Vec3;
+using ColorA = Vec4;
+
+using Point = glm::ivec2;
+
+}
