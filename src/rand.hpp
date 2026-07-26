3fad8ee64fa6dd3d3ccbab09a51e194ec693538f
Stan Serebriakov <i@cfr.pw>
2026-07-26 21:07:50 +0300

    Add PCG option, update README

--- src/rand.hpp
+++ src/rand.hpp
@@ -2,19 +2,34 @@
 
 #include "values.hpp"
 
+#include <bit>
 #include <cstdint>
 #include <limits>
+#include <type_traits>
+
+#ifdef USE_PCG
+#include "pcg_random.hpp"
+#else
 #include <random>
+#endif
 
 namespace raytracer {
 
 class Gen {
-    // TODO: switch to pcg
-    using Engine = std::mt19937_64;
-
     static constexpr int digits = std::numeric_limits<Float>::digits;
-    static constexpr int shift = 64 - digits;
-    static constexpr Float scale = Float(1) / Float(std::uint64_t(1) << digits);
+
+#ifdef USE_PCG
+    using Engine = std::conditional_t<(digits <= 32), pcg32, pcg64>;
+#else
+    using Engine = std::conditional_t<(digits <= 32), std::mt19937, std::mt19937_64>;
+#endif
+    using Result = Engine::result_type;
+
+    static constexpr int bits = std::bit_width(Engine::max());
+    static constexpr int shift = bits - digits;
+    static constexpr Float scale = Float(1) / Float(Result(1) << digits);
+
+    static_assert(digits < bits, "engine output too narrow for Float mantissa");
 
     Engine engine_;
 
